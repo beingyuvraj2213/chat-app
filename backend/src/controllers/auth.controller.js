feae5cd5b499a3255f355444c38bded7ecf00a99
Yuvraj <yuvraj13preet@gmail.com>
2026-05-31 11:07:35 +0530

    Profile Pic Update Option Added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,3 +75,22 @@ export const logout=async(req,res)=>{
         res.status(400).send("Logging Out Unsuccessful")
     }
 }
+
+export const updateProfilePic=async(req,res)=>{
+    try {
+        const {profilePic}=req.body;
+        const userId=req.user._id; 
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile Pic is required"})
+        }
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic)
+        const updateUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
+
+        res.status(200).json(updateUser)
+
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ setServers(["8.8.8.8", "8.8.4.4"])
 
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
@@ -13,9 +14,9 @@ const app=express()
 const PORT=process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
-console.log(process.env.MONGODB_URI)
 app.listen(PORT,()=>{
     console.log("Server running on port ",PORT)
     connectDB()
