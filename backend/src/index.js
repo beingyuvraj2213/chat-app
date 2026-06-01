6aa00aa0d66a3384918fad3af66abfa909b58037
Yuvraj <yuvraj13preet@gmail.com>
2026-06-01 20:29:10 +0530

    Message Route Completed (not the socket part)

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const app=express()
@@ -17,6 +18,8 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
+
 app.listen(PORT,()=>{
     console.log("Server running on port ",PORT)
     connectDB()
